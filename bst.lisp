1cf6c12888e9e8058dc03291650db2c12722bf7d
Darren Ringer <dwringer@gmail.com>
2017-09-13 21:38:52 -0400

    clean up lines and licensing

--- bst.lisp
+++ bst.lisp
@@ -1,3 +1,26 @@
+;;;; BST.LISP - Binary Search Tree template macro
+;;;; Copyright 2017 Darren W. Ringer <dwringer@gmail.com>
+
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use,
+;;;; copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;;; copies of the Software, and to permit persons to whom the
+;;;; Software is furnished to do so, subject to the following
+;;;; conditions:
+
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;;;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;;;; OTHER DEALINGS IN THE SOFTWARE.
 (defpackage :bst
   (:use :common-lisp)
   (:export :make-bst
@@ -10,12 +33,10 @@
 	   :bst-to-list))
 (in-package :bst)
 
-
 (defgeneric bst-insert (x tr))
 (defgeneric bst-member (x tr))
 (defgeneric bst-empty (tr))
 (defgeneric bst-to-list (tr))
-
 
 (defmacro make-bst (&key (element-type 'real) (test #'<) unique-only overwrites)
   (let* ((id (string (gensym "")))
@@ -30,6 +51,7 @@
 	 (left nil :type ,type)
 	 (value nil :type ,elem-type)
 	 (right nil :type ,type))
+       
        (defmethod bst-insert ((x ,element-type) (tr ,struct))
 	 (with-slots ((l left) (v value) (r right)) tr
 	   (cond ((null v) (,constructor :value x))
@@ -50,21 +72,25 @@
 		 (t (if ,overwrites
 			(,constructor :left l :value x :right r)
 			tr)))))
+       
        (defmethod bst-member ((x ,element-type) (tr ,struct))
 	 (with-slots ((l left) (v value) (r right)) tr
 	   (cond ((null v) nil)
 		 ((funcall ,test x v) (when (not (null l)) (bst-member x l)))
 		 ((funcall ,test v x) (when (not (null r)) (bst-member x r)))
 		 (t tr))))
+       
        (defmethod bst-empty ((tr ,struct))
 	 (with-slots ((l left) (v value) (r right)) tr
 	   (and (null v)
 		(or (null l) (bst-empty l))
 		(or (null r) (bst-empty r)))))
+       
        (defmethod bst-to-list ((tr ,struct))
 	 (with-slots ((l left) (v value) (r right)) tr
 	   (concatenate 'list
 			(when (not (null l)) (bst-to-list l))
 			(list v)
 			(when (not (null r)) (bst-to-list r)))))
+       
        (,constructor))))
--- finite-map.lisp
+++ finite-map.lisp
@@ -1,3 +1,26 @@
+;;;; FINITE-MAP.LISP - Finite Map implemented with BST template macro
+;;;; Copyright 2017 Darren W. Ringer <dwringer@gmail.com>
+
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use,
+;;;; copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;;; copies of the Software, and to permit persons to whom the
+;;;; Software is furnished to do so, subject to the following
+;;;; conditions:
+
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;;;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;;;; OTHER DEALINGS IN THE SOFTWARE.
 (load "bst.lisp")
 
 (defpackage :finite-map
@@ -11,12 +34,10 @@
 	   :finite-map-key-not-found))
 (in-package :finite-map)
 
-
 (defgeneric finite-map-bind (k v fm))
 (defgeneric finite-map-lookup (k fm))
 
 (define-condition finite-map-key-not-found (error) (e))
-
 
 (defmacro make-finite-map (&key
  			     (key-element-type 'string)
@@ -43,12 +64,14 @@
        (defstruct ,record-struct
  	 (key nil :type ,key-type)
  	 (value nil :type ,value-element-type))
+
        (defmethod finite-map-bind ((k ,key-element-type)
 				   (v ,value-element-type)
 				   (fm ,struct))
 	 (,constructor
 	  :bst (bst-insert (,record-constructor :key k :value v)
 			   (slot-value fm 'bst))))
+
        (defmethod finite-map-lookup ((k ,key-element-type)
 				     (fm ,struct))
 	 (do* ((branch nil)
@@ -77,6 +100,7 @@
 			  (if (null next-right)
 			      (error 'finite-map-key-not-found)
 			      (setf branch next-right)))))))))
+
        (,constructor
 	:bst (make-bst :element-type ,record-struct
 		       :test #'(lambda (a b) (funcall ,test
